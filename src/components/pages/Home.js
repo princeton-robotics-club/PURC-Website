6308350d664d015816986bd144ecfb19cfe1cf33
Ian Henriques <ih2422@princeton.edu>
2023-06-16 22:04:59 -0400

    Added some tessellated backgrounds + style changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import './Backgrounds.css';
 
 function Home() {
   return (
-    <>
-    </>
+    <div className='body-bg'>
+      <div className='home-bg-img'/>
+      <div className='article-centered'/>
+    </div>
   );
 }
 
--- src/components/pages/Supporters.js
+++ src/components/pages/Supporters.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import './Backgrounds.css';
 
 function Supporters() {
   return (
-    <>
-    </>
+    <div className='body-bg'>
+      <div className='supp-bg-img'/>
+      <div className='article-centered'/>
+    </div>
   );
 }
 
