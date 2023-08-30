836b4dd36adcc0a0c18b3b9a3029e9a4bb208782
lglisic <lana.glisic@gmail.com>
2023-08-30 17:10:29 -0400

    WIP 3

--- src/components/pages/0_home/Home.js
+++ src/components/pages/0_home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../Generic.css';
 import './Home.css';
 
@@ -12,27 +12,27 @@ function Home(props) {
   } = props;
 
   const HERO_WORDS = ['design', 'build', 'wire', 'code', 'test']
-
+  const HERO_SLIDES = ['pacbot-2.jpg', 'droid-1.jpg']
   let wordIndex = 0
   let letterIndex = HERO_WORDS[0].length - 1
   let textTimer = 0
   let forward = false
   let showBar = true
+  let slideIndex = 0
 
-  setInterval(editBar, 500)
-  setInterval(editText, 75)
-
-  function editBar() {
-    if (showBar) {
-      document.getElementById('hero-bar').style.color = 'rgb(231, 117, 0)';
-    } else {
-      document.getElementById('hero-bar').style.color = 'rgba(0, 0, 0, 0)';
+  useEffect(() => {
+    const wordTimer = setInterval(() => {editWord()}, 75);
+    const barTimer = setInterval(() => {editBar()}, 500);
+    const slideTimer = setInterval(() => {showSlides()}, 500);
+    
+    return () => {
+      clearInterval(wordTimer)
+      clearInterval(barTimer)
+      clearInterval(slideTimer)
     }
+  });
 
-    showBar = !showBar
-  }
-
-  function editText() {
+  function editWord() {
     document.getElementById('hero-word').innerHTML = HERO_WORDS[wordIndex].substring(0, letterIndex + 1)
 
     if (letterIndex >= HERO_WORDS[wordIndex].length - 1 && textTimer < 50) {
@@ -64,13 +64,37 @@ function Home(props) {
     }
   }
 
+  function editBar() {
+    if (showBar) {
+      document.getElementById('hero-bar').style.color = 'rgb(231, 117, 0)';
+    } else {
+      document.getElementById('hero-bar').style.color = 'rgba(0, 0, 0, 0)';
+    }
+
+    showBar = !showBar
+  }
+
+  function showSlides() {
+    document.getElementById('hero-img').style.backgroundImage = 'url(../../../images/home/drone-1.jpg);'
+    //document.getElementById('hero-img').style.backgroundImage = `linear-gradient(rgba(231, 117, 0, 0.6),  rgba(3, 162, 220, 0.6), rgba(3, 162, 220, 0.6)), url(../../../images/home/${HERO_SLIDES[slideIndex]});`
+    slideIndex += 1
+
+    if (slideIndex >= HERO_SLIDES.length){
+      slideIndex = 0
+    }
+  }
+
   return (
     <div className='body-bg'>
-     <div className='hero-img'>
-      <div className='hero-logo'></div>
-      <div className='hero-text'><span id='hero-word' className='hero-word'>{HERO_WORDS[0]}</span><span id='hero-bar' className='hero-bar'>|</span>the future</div>
-      <div className='hero-subtext'>princeton university robotics club</div>
-     </div>
+      <div id='hero-img' className='hero-img'>
+        <div className='hero-logo'></div>
+        <div className='hero-text'>
+          <span id='hero-word' className='hero-word'>{HERO_WORDS[0]}</span>
+          <span id='hero-bar' className='hero-bar'>|</span>
+          the&nbsp;&nbsp;future
+        </div>
+        <div className='hero-subtext'>princeton university robotics club</div>
+      </div>
     </div>
   );
 }
