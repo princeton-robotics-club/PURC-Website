93b577c4a95f252af7b2793cbcd5aa2e8a60276e
lglisic <lana.glisic@gmail.com>
2023-08-31 00:48:36 -0400

    Hero section

--- src/components/pages/0_home/Home.js
+++ src/components/pages/0_home/Home.js
@@ -2,8 +2,6 @@ import React, {useEffect} from 'react';
 import '../Generic.css';
 import './Home.css';
 
-import droid1 from '../../../images/home/droid-1.jpg';
-
 // <div className='home-bg-img'/>
 
 function Home(props) {
@@ -19,17 +17,14 @@ function Home(props) {
   let textTimer = 0
   let forward = false
   let showBar = true
-  let slideIndex = 0
 
   useEffect(() => {
     const wordTimer = setInterval(() => {editWord()}, 75);
     const barTimer = setInterval(() => {editBar()}, 500);
-    const slideTimer = setInterval(() => {showSlides()}, 1500);
     
     return () => {
       clearInterval(wordTimer)
       clearInterval(barTimer)
-      clearInterval(slideTimer)
     }
   });
 
@@ -75,32 +70,33 @@ function Home(props) {
     showBar = !showBar
   }
 
-  function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("hero-img")
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none" 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "flex"
-  }
-
   return (
     <div className='body-bg'>
-      <div id='hero-img-2' className='hero-img fade'></div>
-      <div id='hero-img-1' className='hero-img fade'></div>
+      <div className='home-container'>
+        <div className='hero-container'>
+          <div className='hero-img'>
+            <div id='hero-img-6' className='hero-img-content'></div>
+            <div id='hero-img-5' className='hero-img-content'></div>
+            <div id='hero-img-4' className='hero-img-content'></div>
+            <div id='hero-img-3' className='hero-img-content'></div>
+            <div id='hero-img-2' className='hero-img-content'></div>
+            <div id='hero-img-1' className='hero-img-content'></div>
+          </div>
 
-      <div className='hero-no-img'>
-        <div className='hero-logo'></div>
-        <div className='hero-text'>
-          <span id='hero-word' className='hero-word'>{HERO_WORDS[0]}</span>
-          <span id='hero-bar' className='hero-bar'>|</span>
-          the&nbsp;&nbsp;future
+          <div className='hero-no-img'>
+            <div className='hero-logo'></div>
+            <div className='hero-text'>
+              <span id='hero-word' className='hero-word'>{HERO_WORDS[0]}</span>
+              <span id='hero-bar' className='hero-bar'>|</span>
+              the&nbsp;&nbsp;future
+            </div>
+            <div className='hero-subtext'>princeton university robotics club</div>
+          </div>
         </div>
-        <div className='hero-subtext'>princeton university robotics club</div>
-      </div>
 
+        <span style={{color: darkMode ? 'white' : 'black'}}>(HOMEPAGE CONTENT PLACEHOLDER)</span>
+
+      </div>
     </div>
   );
 }
