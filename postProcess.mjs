ff1cdd277988f4a4dbbaa73910d8623177b69f97
Ian Henriques <ih2422@princeton.edu>
2023-09-08 21:46:51 -0400

    Custom build script (use 'npm run prod') to fix routing issues

---
+++ postProcess.mjs
@@ -0,0 +1,5 @@
+import fs from 'fs';
+fs.copyFileSync('build/index.html', 'build/projects.html');
+fs.copyFileSync('build/index.html', 'build/about.html');
+fs.copyFileSync('build/index.html', 'build/supporters.html');
+fs.copyFileSync('build/index.html', 'build/join.html');
