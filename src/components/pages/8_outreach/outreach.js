52c772b5c299fb5e201505ef0d2def25545ddb79
Mahir Majid <mm5764@princeton.edu>
2025-03-17 17:46:31 -0400

    Updates to Outreach Page

--- src/components/pages/8_outreach/outreach.js
+++ src/components/pages/8_outreach/outreach.js
@@ -63,10 +63,93 @@ function Outreach(props) {
                 They provide workshops, hackathons, and other events to teach coding and technology skills.
             </div>
           </div>
-          <div className="img-bgc-speaker-event" />
+          <div className="img-outreach-event" id="img-bgc-event" />
+          </Card>
+
+        <Card
+          darkMode={darkMode}
+          className={
+            "text-box full-width" +
+            (darkMode ? " text-box-dark" : " text-box-light")
+          }
+        >
+          <Link
+            className={
+              "outreach-logo-bgc" +
+              (darkMode ? " outreach-logo-bgc-dark" : " outreach-logo-bgc-light")
+            }
+            to="https://www.firstinspires.org/"
+            title="First Robotics"
+          />
+          <div>
+            <div className="supp-info-text supp-info-text-e-quad">
+              FIRST Robotics encourages young people to explore the world of science, technology, engineering, 
+              and mathematics (STEM) through dynamic robotics competitions. Participants design, build, and 
+              program robots to solve complex challenges, fostering creativity, teamwork, and leadership. 
+              As a non-profit organization, FIRST Robotics provides mentorship and opportunities for students 
+              to develop essential skills for future careers in STEM fields.
+            </div>
+          </div>
+          <div className="img-outreach-event" id="img-first-robotics-event" />
+        </Card>
+
+        <Card
+          darkMode={darkMode}
+          className={
+            "text-box full-width" +
+            (darkMode ? " text-box-dark" : " text-box-light")
+          }
+        >
+          <Link
+            className={
+              "outreach-logo-bgc" +
+              (darkMode ? " outreach-logo-bgc-dark" : " outreach-logo-bgc-light")
+            }
+            to="https://princetonlibrary.org/"
+            title="Princeton Public Library"
+          />
+          <div>
+            <div className="supp-info-text supp-info-text-e-quad">
+            Princeton Public Library is a vibrant community resource dedicated to fostering a love of learning 
+            and reading. With an extensive collection of books, digital media, and educational programs, the 
+            library offers a space for individuals of all ages to explore new ideas and expand their knowledge.
+             As a non-profit institution, Princeton Public Library also hosts cultural events, workshops, and 
+             activities, making it a hub for creativity, connection, and lifelong learning in the Princeton area.
+            </div>
+          </div>
+          <div className="img-outreach-event" id="img-princeton-library-event" />
+        </Card>
+
+        <Card
+          darkMode={darkMode}
+          className={
+            "text-box full-width" +
+            (darkMode ? " text-box-dark" : " text-box-light")
+          }
+        >
+          <Link
+            className={
+              "outreach-logo-bgc" +
+              (darkMode ? " outreach-logo-bgc-dark" : " outreach-logo-bgc-light")
+            }
+            to="https://www.camdencountylibrary.org/riletta-l-cream-ferry-avenue-branch"
+            title="Camden Ferry Library"
+          />
+          <div>
+            <div className="supp-info-text supp-info-text-e-quad">
+            Camden Ferry Library serves as a community hub, offering a wide range of resources, programs, 
+            and services to support learning and personal growth. With a focus on literacy and education, 
+            the library provides access to books, digital media, and educational workshops. As a vital 
+            part of the Camden community, it also hosts events that bring people together and encourage a 
+            love for reading and lifelong learning.
+            </div>
+          </div>
+          <div className="img-outreach-event" id="img-camden-library-event" />
         </Card>
       </section>
+
     </div>
+
   );
 }
 
